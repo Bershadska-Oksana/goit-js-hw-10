8de43fd6b5e95550e7cf2c3829687a280fde2aa4
Oksana Bershadska <ksenaua@icloud.com>
2025-08-03 03:55:31 +0200

    save changes

--- vite.config.js
+++ vite.config.js
@@ -1,49 +1,40 @@
 import { defineConfig } from 'vite';
-import { glob } from 'glob';
+import { resolve } from 'path';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-    base: '/goit-js-hw-10/',
-    root: 'src',
-    build: {
-      sourcemap: true,
-      rollupOptions: {
-        input: glob.sync('./src/*.html'),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
-            }
-            return '[name].js';
-          },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
-        },
+export default defineConfig({
+  base: '/goit-js-hw-10/',
+
+  root: 'src',
+
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        timer: resolve(__dirname, 'src/1-timer.html'),
+        form: resolve(__dirname, 'src/2-form.html'),
       },
-      outDir: '../dist',
-      emptyOutDir: true,
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        entryFileNames: '[name].js',
+      },
     },
-    plugins: [
-      injectHTML(),
-      FullReload(['./src/**/**.html']),
-      SortCss({
-        sort: 'mobile-first',
-      }),
-    ],
-  };
+  },
+
+  plugins: [
+    injectHTML(),
+    FullReload(['./src/**/*.html']),
+    SortCss({
+      sort: 'mobile-first',
+    }),
+  ],
 });
